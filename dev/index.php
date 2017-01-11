3143902dc2a2d49768f46147cf77948cbfa853e1
Kaio <kaio.teoi7@gmail.com>
2017-01-11 15:34:36 -0200

    commit #2

--- dev/index.php
+++ dev/index.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Kaio Teoi - Web developer</title>
-</head>
-<body>
-	
-</body>
+	<head>
+		<title>Kaio Teoi</title>
+		<meta charset="utf-8">
+        <meta content="IE=edge" http-equiv="X-UA-Compatible">
+        <meta content="width=device-width, initial-scale=1, user-scalable=no" name="viewport">
+        <meta content="pt-br" http-equiv="content-language">
+        <meta content="#000000" name="theme-color">
+        <!-- <meta content="Site about a web developer working hard from zero to hero. Come in, you're welcome." name="description"> -->
+        <meta content="" name="description">
+        <meta content="Desenvolvedor, Web, Programador, Portfolio, Kaio, Teoi" name="keywords">
+        <meta content="São Paulo - SP" name="geo.placename">
+        <meta content="BR-SP" name="geo.region">
+        <meta content="1 week" name="revisit-after">
+        <meta content="index, follow" name="robots">
+        <meta content="index, follow" name="googlebot">
+
+		<link rel="icon" type="image/png" href="images/blacktriangle_favicon.png">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+	</head>
+
+	<body>
+
+	</body>
 </html>
